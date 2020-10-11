dbd38628f4cf4ec2d8dd9f607114dad40e3429c7
Joshua Behrens <behrens@heptacom.de>
2020-10-11 19:38:07 +0200

    Add services.yml loading
    FOSS-11

--- src/SymfonyBundle.php
+++ src/SymfonyBundle.php
@@ -6,6 +6,15 @@ if (\class_exists(\Symfony\Component\HttpKernel\Bundle\Bundle::class)) {
     class SymfonyBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
     {
         protected $name = 'HeptacomOpenAuth';
+
+        public function build(\Symfony\Component\DependencyInjection\ContainerBuilder $container): void
+        {
+            parent::build($container);
+
+            $fileLocator = new \Symfony\Component\Config\FileLocator($this->getPath());
+            (new \Symfony\Component\DependencyInjection\Loader\YamlFileLoader($container, $fileLocator))
+                ->load($this->getPath() . '/Resources/config/services.yml');
+        }
     }
 } else {
     class SymfonyBundle
